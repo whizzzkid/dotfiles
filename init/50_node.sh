b37127beec710687cfe41fca23142a2157a4a777
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2024-02-26 00:26:36 -0700

    chore(configs): 🔖 Saving State
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- init/50_node.sh
+++ init/50_node.sh
@@ -1,5 +1,2 @@
-# Load npm-related functions.
-source $DOTFILES/source/50_node.sh init
-
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
--- init/50_ruby.sh
+++ init/50_ruby.sh
@@ -1,16 +1,2 @@
-# Initialize rbenv.
-source $DOTFILES/source/50_ruby.sh
-
 # Install Ruby.
-if [[ "$(type -P rbenv)" ]]; then
-  versions=(2.4.0)
-
-  rubies=($(setdiff "${versions[*]}" "$(rbenv whence ruby)"))
-  if (( ${#rubies[@]} > 0 )); then
-    e_header "Installing Ruby versions: ${rubies[*]}"
-    for r in "${rubies[@]}"; do
-      rbenv install "$r"
-      [[ "$r" == "${versions[0]}" ]] && rbenv global "$r"
-    done
-  fi
-fi
+curl -sSL https://get.rvm.io | bash -s stable --ruby
